c26b833ee358337ffe14f619f6f92958b0443717
1d4ducks <hellocarrierpidgeon@gmail.com>
2020-12-02 20:05:13 +0000

    Day 2 Puzzle 2

--- day2_password_philosophy.py
+++ day2_password_philosophy.py
@@ -6,6 +6,12 @@ passwords = [password.replace(':', ' ').replace('-', ' ').split() for password i
 
 counter = 0
 
+'''
+Puzzle 1
+I solved it by splitting the input into its 4 parts then checking whether the target letter
+appeared in the password. 
+If yes, count how many times it appears and compare check if it follows the constraint.
+'''
 for password in passwords:
     low_constraint = password[0]
     high_constraint = password[1]
@@ -18,4 +24,26 @@ for password in passwords:
         False
     False
 
+'''
+Puzzle 2
+I solved it by using the same splitting idea from above and only working on the passwords 
+i know contain the target letter
+Here i find the index of where that letter appears in the password and then use boolean
+logic to apply the puzzle rules
+'''
+second_counter = 0
+
+for password in passwords:
+    low_constraint = password[0]
+    high_constraint = password[1]
+    target = password[2]
+    full_pass = password[3]
+    if target in full_pass:
+        idxs = [idx for idx, char in enumerate(full_pass) if char == target]
+        if int(low_constraint)-1 in idxs and int(high_constraint)-1 in idxs:
+            False
+        elif int(low_constraint)-1 not in idxs and int(high_constraint)-1 not in idxs:
+            False
+        else:
+            second_counter += 1
 
